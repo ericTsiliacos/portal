ed10f615c6bef49ea5b24b2fe36b41599dfb2a5f
Eric Tsiliacos <erictsiliacos@gmail.com>
2021-02-13 11:26:00 -0800

    Extract test helpers for portal package

---
+++ internal/portal/helpers_test.go
@@ -0,0 +1,82 @@
+package portal
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func SetupBareGitProject(t *testing.T, rootDirectory string) {
+	t.Helper()
+
+	projectPath := filepath.Join(rootDirectory, "project")
+	check(os.Mkdir(projectPath, 0755))
+
+	check(os.Chdir(projectPath))
+
+	_, err := exec.Command("git", "init", "--bare").Output()
+	check(err)
+
+	check(os.Chdir(rootDirectory))
+
+	_, err = exec.Command("git", "clone", "project", "setup", "--progress").Output()
+	check(err)
+
+	setupPath := filepath.Join(rootDirectory, "setup")
+	check(os.Chdir(setupPath))
+
+	_, err = exec.Command("git", "commit", "--allow-empty", "--message", "\"initial commit\"").Output()
+	check(err)
+
+	_, err = exec.Command("git", "push", "origin", "head", "--progress").Output()
+	check(err)
+
+	check(os.Chdir(rootDirectory))
+}
+
+func SetupProjectClone(t *testing.T, rootDirectory string) string {
+	t.Helper()
+
+	_, err := exec.Command("git", "clone", "project", "clone1", "--progress").Output()
+	check(err)
+
+	clonePath := filepath.Join(rootDirectory, "clone1")
+	check(os.Chdir(clonePath))
+
+	return clonePath
+}
+
+func RemoteBranchExists(t *testing.T, branch string) bool {
+	t.Helper()
+
+	remoteBranch, err := exec.Command("git", "ls-remote", "--heads", "origin", branch).Output()
+	check(err)
+	return len(remoteBranch) > 0
+}
+
+func LocalBranchExists(t *testing.T, branch string) bool {
+	t.Helper()
+
+	localBranch, err := exec.Command("git", "branch", "--list", branch).Output()
+	check(err)
+
+	return len(localBranch) > 0
+}
+
+func CleanIndex(t *testing.T) bool {
+	t.Helper()
+
+	index, err := exec.Command("git", "status", "--porcelain=v1").Output()
+	check(err)
+
+	indexCount := strings.Count(string(index), "\n")
+	return !(indexCount > 0)
+}
--- internal/portal/push_test.go
+++ internal/portal/push_test.go
@@ -4,9 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 	"testing"
 	"time"
 
@@ -14,85 +11,51 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+func TestPortalPushSaga(t *testing.T) {
+	rootDirectory := t.TempDir()
+
+	SetupBareGitProject(t, rootDirectory)
+
+	check(os.Chdir(SetupProjectClone(t, rootDirectory)))
+
+	fileName := "foo"
+	fileHandle, err := os.Create(fileName)
+	check(err)
+	defer fileHandle.Close()
+
+	now := time.Now().Format(time.RFC3339)
+	portalBranch := "pa-ir-portal"
+	steps := PushSagaSteps(portalBranch, now, "v1.0.0", true)
+	saga := saga.Saga{Steps: steps, Verbose: testing.Verbose()}
+	errors := saga.Run()
+
+	assert.Empty(t, errors)
+	assert.NoFileExists(t, fileName)
+	assert.True(t, RemoteBranchExists(t, portalBranch))
+	assert.False(t, LocalBranchExists(t, portalBranch))
+	assert.True(t, CleanIndex(t))
+}
+
+func TestPortalPushSagaWithFailures(t *testing.T) {
+	now := time.Now().Format(time.RFC3339)
+	portalBranch := "pa-ir-portal"
+	steps := PushSagaSteps(portalBranch, now, "v1.0.0", true)
+
+	for i := 1; i < len(steps); i++ {
+		fmt.Printf("test run %d:", i)
+		fmt.Println()
+		fmt.Println("-----------------")
+
+		testPortalPushSagaFailure(t, portalBranch, now, i)
 	}
 }
 
-func setupBareGitProject(t *testing.T, rootDirectory string) {
-	t.Helper()
-
-	projectPath := filepath.Join(rootDirectory, "project")
-	check(os.Mkdir(projectPath, 0755))
-
-	check(os.Chdir(projectPath))
-
-	_, err := exec.Command("git", "init", "--bare").Output()
-	check(err)
-
-	check(os.Chdir(rootDirectory))
-
-	_, err = exec.Command("git", "clone", "project", "setup", "--progress").Output()
-	check(err)
-
-	setupPath := filepath.Join(rootDirectory, "setup")
-	check(os.Chdir(setupPath))
-
-	_, err = exec.Command("git", "commit", "--allow-empty", "--message", "\"initial commit\"").Output()
-	check(err)
-
-	_, err = exec.Command("git", "push", "origin", "head", "--progress").Output()
-	check(err)
-
-	check(os.Chdir(rootDirectory))
-}
-
-func setupProjectClone(t *testing.T, rootDirectory string) string {
-	t.Helper()
-
-	_, err := exec.Command("git", "clone", "project", "clone1", "--progress").Output()
-	check(err)
-
-	clonePath := filepath.Join(rootDirectory, "clone1")
-	check(os.Chdir(clonePath))
-
-	return clonePath
-}
-
-func remoteBranchExists(t *testing.T, branch string) bool {
-	t.Helper()
-
-	remoteBranch, err := exec.Command("git", "ls-remote", "--heads", "origin", branch).Output()
-	check(err)
-	return len(remoteBranch) > 0
-}
-
-func localBranchExists(t *testing.T, branch string) bool {
-	t.Helper()
-
-	localBranch, err := exec.Command("git", "branch", "--list", branch).Output()
-	check(err)
-
-	return len(localBranch) > 0
-}
-
-func cleanIndex(t *testing.T) bool {
-	t.Helper()
-
-	index, err := exec.Command("git", "status", "--porcelain=v1").Output()
-	check(err)
-
-	indexCount := strings.Count(string(index), "\n")
-	return !(indexCount > 0)
-}
-
-func testSagaFailure(t *testing.T, portalBranch string, now string, index int) {
+func testPortalPushSagaFailure(t *testing.T, portalBranch string, now string, index int) {
 	rootDirectory := t.TempDir()
 
-	setupBareGitProject(t, rootDirectory)
+	SetupBareGitProject(t, rootDirectory)
 
-	check(os.Chdir(setupProjectClone(t, rootDirectory)))
+	check(os.Chdir(SetupProjectClone(t, rootDirectory)))
 
 	fileName := "foo"
 	fileHandle, err := os.Create(fileName)
@@ -113,46 +76,7 @@ func testSagaFailure(t *testing.T, portalBranch string, now string, index int) {
 
 	assert.NotEmpty(t, errors)
 	assert.FileExists(t, fileName)
-	assert.False(t, remoteBranchExists(t, portalBranch))
-	assert.False(t, localBranchExists(t, portalBranch))
-	assert.False(t, cleanIndex(t))
-}
-
-func TestPortalPushSaga(t *testing.T) {
-	rootDirectory := t.TempDir()
-
-	setupBareGitProject(t, rootDirectory)
-
-	check(os.Chdir(setupProjectClone(t, rootDirectory)))
-
-	fileName := "foo"
-	fileHandle, err := os.Create(fileName)
-	check(err)
-	defer fileHandle.Close()
-
-	now := time.Now().Format(time.RFC3339)
-	portalBranch := "pa-ir-portal"
-	steps := PushSagaSteps(portalBranch, now, "v1.0.0", true)
-	saga := saga.Saga{Steps: steps, Verbose: testing.Verbose()}
-	errors := saga.Run()
-
-	assert.Empty(t, errors)
-	assert.NoFileExists(t, fileName)
-	assert.True(t, remoteBranchExists(t, portalBranch))
-	assert.False(t, localBranchExists(t, portalBranch))
-	assert.True(t, cleanIndex(t))
-}
-
-func TestPortalPushSagaWithFailures(t *testing.T) {
-	now := time.Now().Format(time.RFC3339)
-	portalBranch := "pa-ir-portal"
-	steps := PushSagaSteps(portalBranch, now, "v1.0.0", true)
-
-	for i := 1; i < len(steps); i++ {
-		fmt.Printf("test run %d:", i)
-		fmt.Println()
-		fmt.Println("-----------------")
-
-		testSagaFailure(t, portalBranch, now, i)
-	}
+	assert.False(t, RemoteBranchExists(t, portalBranch))
+	assert.False(t, LocalBranchExists(t, portalBranch))
+	assert.False(t, CleanIndex(t))
 }
